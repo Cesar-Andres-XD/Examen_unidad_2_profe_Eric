8ce5a54d5e6baf1b2157fabc97945caae029c4a9
AlanEA <omar.encinas.cetis69@gmail.com>
2023-10-27 07:46:08 -0700

    Agregar archivos de proyecto.

---
+++ Examen/Ejercicios.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Examen_unidad_2_profe_Eric.Examen
+{
+    internal class Ejercicios
+    {
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Examen_unidad_2_profe_Eric
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            
+        }
+    }
+}
